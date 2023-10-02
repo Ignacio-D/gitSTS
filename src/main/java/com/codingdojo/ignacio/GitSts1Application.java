6b6489d351e4ac646eff8f320c44c244c46ec13c
Ignacio Duran <ignaciodv2802@gmail.com>
2023-10-02 02:24:31 -0300

    Proyecto Base

---
+++ src/main/java/com/codingdojo/ignacio/GitSts1Application.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ignacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSts1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSts1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/ignacio/GitSts1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ignacio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSts1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
